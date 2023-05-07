530d86432e63caf9f8736587a75531f094b79565
hoopmad <hoopmad22@gmail.com>
2023-05-07 22:10:16 +0900

    updated photo predict app

--- ndjango/refrigerators/photo_predict/photo_predict.py
+++ ndjango/refrigerators/photo_predict/photo_predict.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from tensorflow import keras
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
 import numpy as np
 
 app = Flask(__name__)
@@ -16,20 +15,30 @@ def predict():
     img = image.load_img(image_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    preds = model.predict(x)
-    prediction = decode_predictions(preds, top=3)[0]
+    images = np.vstack([x])
+    pred = model.predict(images, batch_size=32)
+    label = np.argmax(pred, axis=1)
 
     # Convert the prediction to a list of dictionaries
-    results = []
-    for pred in prediction:
-        results.append({'name': pred[1], 'score': float(pred[2])})
+    result = {'name': class_names_kor[label[0]], 'score':float(pred[0][label])}
 
     # Return the prediction in JSON format
-    return jsonify({'results': results})
+    return jsonify({'result': result})
 
 if __name__ == '__main__':
     # Load the machine learning model
-    model = keras.models.load_model('./resnet50.h5')
+    model = keras.models.load_model('./best-cnn-model.h5')
+    
+    class_names = ['apple', 'banana', 'beetroot', 'bell pepper', 'cabbage', 'carrot', 'cauliflower', 'chilli pepper', 'corn', 'cucumber',
+                   'eggplant', 'garlic', 'ginger', 'grapes', 'jalepeno', 'kiwi', 'lemon', 'lettuce', 'mango', 'onion', 
+                   'orange', 'paprika', 'pear', 'peas', 'pineapple', 'pomegranate', 'potato', 'radish', 'soy beans', 'spinach', 
+                   'sweetcorn', 'sweetpotato', 'tomato', 'turnip', 'watermelon']
+    
+    class_names_kor = {
+        0: '사과', 1: '바나나', 2: '비트', 3: '파프리카', 4: '양배추', 5: '당근', 6: '컬리플라워', 7: '고추', 8: '옥수수', 9: '오이',
+        10: '가지', 11: '마늘', 12: '생강', 13: '포도', 14: '할라피뇨', 15: '키위', 16: '레몬', 17: '양상추', 18: '망고', 19: '양파',
+        20: '오렌지', 21: '파프리카', 22: '배', 23: '완두콩', 24: '파인애플', 25: '석류', 26: '감자', 27: '무', 28: '콩', 29: '시금치',
+        30: '초당옥수수', 31: '고구마', 32: '토마토', 33: '순무', 34: '수박'
+    }
     
     app.run()
